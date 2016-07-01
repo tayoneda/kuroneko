dbc44393722e5a0117c94314ce2844c39a396738
Taka Yoneda <tayoneda@yahoo.com>
2016-07-01 23:59:48 +0900

    modified a backing bean

--- kuroneko/src/com/gunome/kuroneko/beans/NekoBean.java
+++ kuroneko/src/com/gunome/kuroneko/beans/NekoBean.java
@@ -1,7 +1,9 @@
 package com.gunome.kuroneko.beans;
 
+import java.sql.Timestamp;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -13,16 +15,41 @@ import com.gunome.kuroneko.entity.Neko;
 @RequestScoped
 public class NekoBean {
 	
+	private String name;
+	private Timestamp dateBirth;
+	
 	private List<Neko> nekoList;
 	
+	/**
+	 * ejb to access cat data
+	 */
 	@Inject
 	private NekoFacade nekoFacade;
+	
+	/**
+	 * initiate a view
+	 */
+	@PostConstruct
+	public void init() {
+		getAllNeko();
+	}
 	
 	public String date() {
 		return (new java.util.Date()).toString();
 	}
 	
-	private void findAllNeko() {
+	/**
+	 * create a new cat
+	 */
+	public void createNeko() {
+		if (name.isEmpty() && dateBirth != null) {
+			
+		}
+	}
+	/**
+	 * get all cats registered
+	 */
+	private void getAllNeko() {
 		nekoList = nekoFacade.findAll();
 	}
 
@@ -32,5 +59,21 @@ public class NekoBean {
 
 	public void setNekoList(List<Neko> nekoList) {
 		this.nekoList = nekoList;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Timestamp getDateBirth() {
+		return dateBirth;
+	}
+
+	public void setDateBirth(Timestamp dateBirth) {
+		this.dateBirth = dateBirth;
 	}
 }
